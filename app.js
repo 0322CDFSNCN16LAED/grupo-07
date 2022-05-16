e43401543885994ea90cb52a0baae78283e58225
RubenTomasella <rubentomasella@gmail.com>
2022-05-16 16:37:15 -0300

    Agregado de carpeta Public con imagenes y css, mas footer basico

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const path=require('path')
+
+
+const PORT = 3000;
+
+app.listen(PORT,()=> console.log("Estamos corriendo en el puerto:" + PORT));
+
+app.use(express.static(path.join(__dirname,"/public")));
+
+
+
+
+app.get('/',(req,res)=>{
+
+    res.sendFile(path.join(__dirname,"/views/index.html"))
+})
