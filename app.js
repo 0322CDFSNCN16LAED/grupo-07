5ecd2645d4746a6aa8011c1f45d46b8ef5cb083e
Delfina Callegari <delfinacallegari@gmail.com>
2022-05-21 13:23:44 -0300

    changes header

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 3333;
 app.listen(PORT, () => {
   console.log("Estamos corriendo en el puerto " + PORT);
 });
@@ -16,4 +16,32 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/home.html"));
 });
 
+app.get("/login", (req, res) => {
+  // funcion controladora o handler
+  res.sendFile(path.join(__dirname, "views/login.html"));
+});
 
+app.get("/register", (req, res) => {
+  // funcion controladora o handler
+  res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+app.get("/productos", (req, res) => {
+  // funcion controladora o handler
+  res.sendFile(path.join(__dirname, "views/productos.html"));
+});
+
+app.get("/escuelas-de-surf", (req, res) => {
+  // funcion controladora o handler
+  res.sendFile(path.join(__dirname, "views/escuelas-de-surf.html"));
+});
+
+app.get("/nosotros", (req, res) => {
+  // funcion controladora o handler
+  res.sendFile(path.join(__dirname, "views/nosotros.html"));
+});
+
+app.get("/carrito", (req, res) => {
+  // funcion controladora o handler
+  res.sendFile(path.join(__dirname, "views/carrito.html"));
+});
